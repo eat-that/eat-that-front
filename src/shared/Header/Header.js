1de39406546a1999fd212bd2ee1e9f93ee5b6e46
Florian <florian.grember@gmail.com>
2020-10-21 17:19:24 +0200

    header

--- src/shared/Header/Header.js
+++ src/shared/Header/Header.js
@@ -2,13 +2,82 @@ import React from 'react'
 import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from '@material-ui/icons/Search'
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import PersonIcon from '@material-ui/icons/Person';
+import FilterListIcon from '@material-ui/icons/FilterList';
 
+const useStyles = makeStyles((theme) => ({
+    navBar:{
+        display:'flex',
+        flexDirection:'column',
+        width:'100%'
+    },
+
+    row1:{
+        display:'flex',
+        justifyContent:'space-between'
+    },
+
+    row2:{
+        display:'flex',
+        justifyContent:'space-between'
+    },
+
+    inputContainer:{
+        width:'90%'
+    },
+    input:{
+        color:'white',
+        width:'80%'
+    },
+
+
+    button:{
+        color:'white',
+    },
+    icon:{
+        color:'white'
+    },
+}));
 const Header = () => {
-console.log('here')
+
+    const classes = useStyles();
+
     return(
         <AppBar position='static'>
             <Toolbar>
-                <EmojiPeopleIcon/>
+                <div className={classes.navBar}>
+
+                    <div className={classes.row1}>
+                        <Button className={classes.button}>
+                            <EmojiPeopleIcon className={classes.icon}/>
+                            Votre position
+                        </Button>
+                        <IconButton className={classes.icon}>
+                            <PersonIcon/>
+                        </IconButton>
+                    </div>
+
+                <div className={classes.row2}>
+                    <span className={classes.inputContainer}>
+                        <InputBase
+                            className={classes.input}
+                              placeholder="Que souhaitez-vous manger ?"
+                          />
+                        <IconButton type="submit" className={classes.icon}>
+                            <SearchIcon />
+                        </IconButton>
+                    </span>
+                    <IconButton className={classes.icon}>
+                        <FilterListIcon/>
+                    </IconButton>
+                </div>
+
+                </div>
             </Toolbar>
         </AppBar>
     )
